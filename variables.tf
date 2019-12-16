47b2482d8aa921656e489e053eba89469e46e43d
Sagar P Ghagare <sagar.ghagare7@gmail.com>
2019-12-16 10:59:33 +0000

    varibles file added

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "environment" {
+  description = "The environment this is deployed for"
+}
+
+variable "slack_webhook_url" {
+  description = "The Slack webhook URL to send messages to"
+}
+
+variable "icon_emoji" {
+  description = "The emoji to use as the avatar in Slack"
+}
+
